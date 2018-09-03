3916016062768f56c7e66d48627384b8a2bf6a97
Sergey Vecher <s_vecher@ngs.ru>
2018-09-03 19:56:47 +0700

    HW Lesson 6, MapSearchKeyResumeStorage class added

---
+++ src/com/javawebinar/basejava/webapp/storage/MapSearchKeyResumeStorage.java
@@ -0,0 +1,57 @@
+package com.javawebinar.basejava.webapp.storage;
+
+import com.javawebinar.basejava.webapp.model.Resume;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MapSearchKeyResumeStorage extends AbstractStorage {
+    private Map<Object, Resume> map = new TreeMap<>();
+
+    @Override
+    protected void toUpdate(Resume resume, Object searchKey) {
+        map.put(resume.getUuid(), resume);
+    }
+
+    @Override
+    protected void toSave(Resume resume, Object searchKey) {
+        map.put(resume.getUuid(), resume);
+    }
+
+    @Override
+    protected Resume toGet(Object resume) {
+        return (Resume) resume;
+    }
+
+    @Override
+    protected void toDelete(Object resume) {
+        map.remove(((Resume) resume).getUuid());
+    }
+
+    @Override
+    public void clear() {
+        map.clear();
+    }
+
+    @Override
+    public List<Resume> getAllSorted() {
+        return new ArrayList<>(map.values());
+    }
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+
+    @Override
+    protected boolean isExist(Object resume) {
+        return resume != null;
+    }
+
+    @Override
+    protected Resume getSearchKey(String uuid) {
+        return map.get(uuid);
+    }
+}
---
+++ test/com/javawebinar/basejava/webapp/storage/MapSearchKeyResumeStorageTest.java
@@ -0,0 +1,7 @@
+package com.javawebinar.basejava.webapp.storage;
+
+public class MapSearchKeyResumeStorageTest extends AbstractStorageTest {
+    public MapSearchKeyResumeStorageTest() {
+        super(new MapSearchKeyResumeStorage());
+    }
+}
